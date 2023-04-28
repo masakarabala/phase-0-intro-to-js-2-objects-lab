08ef99e0ae3ab61dbbb22a37fee8962655bd2c2e
masakarabala <48476935+masakarabala@users.noreply.github.com>
2023-04-28 20:21:06 +0200

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution in this file!
+const employee ={
+    name:'Sammi' ,
+     streetAddress:'9 Broadway'
+};
+console.log(employee)
+/*const employee ={};
+    employee.name ="Sam";// or employee["name"] = "Sam";
+    employee.streetAddress ="11 Broadway";*/
+
+    
+    function updateEmployeeWithKeyAndValue(employee , key ,value){
+        const newEmployee = {...employee, name:"Sam", streetAddress:"11 Broadway"};
+        return newEmployee;
+    }
+    //console.log(updateEmployeeWithKeyAndValue())
+
+    
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value; 
+      
+        return employee;
+      }
+      const employee1 = destructivelyUpdateEmployeeWithKeyAndValue(employee, "name" ,"momo");
+      //console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee1))
+
+      
+      function deleteFromEmployeeByKey(employee , key ){
+        const employee2 = {...employee[key]};
+        delete employee2[key];
+        return employee2;
+    }
+
+    function destructivelyDeleteFromEmployeeByKey(employee, key) {
+        delete employee[key];
+        return employee;
+      }
+      
+   
+
+    
+    
+    
